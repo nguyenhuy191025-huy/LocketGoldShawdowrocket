86aac378bf0697db468d2954ed3d20a75fc8389d
nguyenhuy191025-huy <nguyenhuy191025@gmail.com>
2026-07-29 05:02:58 +0700

    Update deleteHeader.js

--- deleteHeader.js
+++ deleteHeader.js
@@ -1,13 +1,4 @@
-#!url=https:
-#!name=Nguyenhuy191025
-#!desc=Crack By nhuy
-
-[Script]
-# ~ By Vuong2023
-# ～ Egern transfer to Shadowrocket & Surge & LanceX
-revenuecat = type=http-response, pattern=^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$), script-path=https://raw.githubusercontent.com/vuong2023/shad/main/js/Locket_Ohoang7.js, requires-body=true, max-size=-1, timeout=60
-
-deleteHeader = type=http-request, pattern=^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers), script-path=https://raw.githubusercontent.com/vuong2023/shad/main/js/deleteHeader.js, timeout=60
-
-[MITM]
-hostname = %APPEND% api.revenuecat.com
+// deleteHeader.js
+if ($request.headers['X-RevenueCat-ETag']) { delete $request.headers['X-RevenueCat-ETag']; }
+if ($request.headers['x-revenuecat-etag']) { delete $request.headers['x-revenuecat-etag']; }
+$done({headers: $request.headers});
