83265349f965a5176e4f660f213b6e90d4c2c5e8
nguyenhuy191025-huy <nguyenhuy191025@gmail.com>
2026-07-29 05:03:23 +0700

    Update Locket_.js

--- Locket_.js
+++ Locket_.js
@@ -1,13 +1,13 @@
-#!url=https:
-#!name=nguyenhuy191025
-#!desc=Crack By nhuy
-
-[Script]
-# ~ By Vuong2023
-# ～ Egern transfer to Shadowrocket & Surge & LanceX
-revenuecat = type=http-response, pattern=^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$), script-path=https://raw.githubusercontent.com/vuong2023/shad/main/js/Locket_Ohoang7.js, requires-body=true, max-size=-1, timeout=60
-
-deleteHeader = type=http-request, pattern=^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers), script-path=https://raw.githubusercontent.com/vuong2023/shad/main/js/deleteHeader.js, timeout=60
-
-[MITM]
-hostname = %APPEND% api.revenuecat.com
+// Locket Gold Mock Script
+var obj = JSON.parse($response.body);
+obj.subscriber.entitlements["Gold"] = {
+  "expires_date": "2099-02-18T07:52:54Z",
+  "purchase_date": "2020-02-11T07:52:54Z",
+  "product_identifier": "com.locket.Locket.premium.yearly"
+};
+obj.subscriber.subscriptions["com.locket.Locket.premium.yearly"] = {
+  "expires_date": "2099-02-18T07:52:54Z",
+  "purchase_date": "2020-02-11T07:52:54Z",
+  "period_type": "normal"
+};
+$done({body: JSON.stringify(obj)});
